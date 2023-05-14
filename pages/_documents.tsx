e81c80a84e90c8b39d8c0bb9047e9e4861618119
Gabriel Sens <gabriel_sens@hotmail.com>
2023-05-14 11:16:22 -0300

    Inicialização do Material UI

---
+++ pages/_documents.tsx
@@ -0,0 +1,24 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+        </Head>
+      </Html>
+    );
+  }
+}
