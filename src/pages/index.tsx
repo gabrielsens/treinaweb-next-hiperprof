fb71cf7078cb4a37301f10f01a1be30678b6dcf3
Gabriel Sens <gabriel_sens@hotmail.com>
2023-05-15 23:19:42 -0300

    Criando page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
 import PageTitle from "@components/data-display/PageTitle";
+import { Button, Icon, TextField } from "@mui/material";
+import { BoxButtons, HomeContainer } from "@styles/pages/index.styles";
 
 export default function Home() {
   return (
-    <div>
-      <PageTitle title="title" subtitle="exemplo" />
-    </div>
+    <HomeContainer>
+      <PageTitle
+        title="Encontre o professor ideal para você!"
+        subtitle="Pesquise pelo professor ideal para você!"
+      />
+      <TextField
+        label="Encontre um professor"
+        fullWidth
+        InputProps={{
+          startAdornment: <Icon>search</Icon>
+        }}
+        required
+        sx={{ mt: 3, mb: 3 }}
+      />
+      <BoxButtons>
+        <Button type="submit" variant="contained">Buscar o professor</Button>
+      </BoxButtons>
+    </HomeContainer>
   )
 }
