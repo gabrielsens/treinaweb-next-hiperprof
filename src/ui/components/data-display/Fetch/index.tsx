7f30f96c56b583a09232ba274daf1d9f210d81c2
Gabriel Sens <gabriel_sens@hotmail.com>
2023-05-24 23:15:54 -0300

    Componente loading

---
+++ src/ui/components/data-display/Fetch/index.tsx
@@ -0,0 +1,36 @@
+import { Box, CircularProgress, Typography } from "@mui/material";
+import { type } from "os";
+import { ReactElement } from "react";
+
+interface FetchProps<T> {
+  data: T[] | undefined;
+  render: (dataFiltered: T[]) => ReactElement;
+  message: string,
+  maxLength?: number
+}
+
+type FetchComponent = <T>(props: FetchProps<T>) => ReactElement;
+
+const Fetch: FetchComponent = ({ data, render, message, maxLength = 3 }) => {
+  if (data) {
+    const dataFilted = data.slice(0, maxLength);
+
+    if (data.length === 0) {
+      return (
+        <Typography>
+          {message}
+        </Typography>
+      )
+    }
+
+    return render(dataFilted);
+  }
+
+  return (
+    <Box>
+      <CircularProgress />
+    </Box>
+  )
+};
+
+export default Fetch;
