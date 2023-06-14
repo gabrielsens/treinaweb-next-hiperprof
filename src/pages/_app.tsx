d1b699459fccf7046f23cf835d4b8efb670df638
Gabriel Sens <gabriel_sens@hotmail.com>
2023-06-13 22:28:49 -0300

    tema

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+import Base from '@components/surfaces/Base'
+import { ThemeProvider } from '@mui/material'
 import '@styles/globals.css'
 import type { AppProps } from 'next/app'
+import light from 'ui/theme/light-theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={light}>
+      <Base>
+        <Component {...pageProps} />
+      </Base>
+    </ThemeProvider>
+  );
 }
